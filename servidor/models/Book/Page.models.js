e084d4e4add6f535ae28744aefbaf2f802d8e53a
Lucia Astray <lu@MacBook-Pro-de-Lucia.local>
2019-06-12 10:18:36 +0200

    rutas 2

--- servidor/models/Book/Page.models.js
+++ servidor/models/Book/Page.models.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const pageSchema = new Schema({
-  Book: { type: Schema.Types.ObjectId, ref: 'Book' },
+  book: { type: Schema.Types.ObjectId, ref: 'Book' },
   texts: [{ type: Schema.Types.ObjectId, ref: 'TextPage' }],
   imageBackground: { type: Schema.Types.ObjectId, ref: 'ImagePage' },
   imageCharacter: [{ type: Schema.Types.ObjectId, ref: 'ImagePage' }],
--- servidor/routes/book.routes.js
+++ servidor/routes/book.routes.js
@@ -3,18 +3,40 @@ const router = express.Router()
 
 const Book = require('../models/Book/Book.model')
 
+
+//Crear un cuento nuevo
 router.post('/newBook', (req, res) => {
   Book.create(req.body)
     .then(data => res.json(data))
     .catch(err => console.log('Error:', err))
 })
 
+
+//lista de cuentos
 router.get('/', (req, res) => {
   Book.find()
     .then(data => res.json(data))
     .catch(err => console.log('Error:', err))
 })
 
+
+//detalles cuento
+router.get('/:id', (req, res) => {
+  const id = req.params.id
+  Book.findById(id)
+    .then(data => res.jason(data))
+    .catch(err => console.log('Error:', err))
+})
+
+
+//añadir una pagina al cuento
+router.post('/addPage/:id', (req, res) => {  //id del book al que pertenece
+  const { id } = req.params;
+  const newPage = { positionX, positionY } = req.body;
+  newPage.book = id;   //le guardamos el id del book al que pertenece
+})
+
+router.post('/addText/:i')
 
 
 module.exports = router;
