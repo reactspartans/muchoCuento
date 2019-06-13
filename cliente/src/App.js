29bd10fdfabbf1680e835a9f88aaa1e6ea4d2780
LadyCrispy <cvg.94@hotmail.com>
2019-06-13 17:04:08 +0200

    signup con detec. usuario logueado

--- cliente/src/App.js
+++ cliente/src/App.js
@@ -5,18 +5,38 @@ import { Switch, Route } from 'react-router-dom'
 import { Index } from './components/Inicio/Index';
 import { NavBar } from './components/NavBar';
 import {Login, SingUp, Logout} from './components/Inicio/auth/logForms'
+import AuthServices from './services/auth-services'
+
+
 
 export default class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { loggedInUser: null }
+    this.services = new AuthServices()
+  }
+
+  setUser = userObj => this.setState({ loggedInUser: userObj })
+
+  fetchUser = () => {
+    if (this.state.loggedInUser === null) {
+      this.services.loggedin()
+        .then(response => this.setState({ loggedInUser: response }))
+        .catch(x => this.setState({ loggedInUser: false }))
+    }
+  }
+
+
+
     render() {
       return (
         <main>
 
-          <NavBar/>
+          <NavBar setTheUser={this.setUser}/>
 
           <Switch>
             <Route path="/" exact component={Index} />
             <Route path="/tales-editor" exact component={TalesEditor} />
-           
           </Switch>
         
 
