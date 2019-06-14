bd7495002b19165c7f282d14fc6a20cc6f012f65
Lucia Astray <lu@MacBook-Pro-de-Lucia.local>
2019-06-14 09:35:33 +0200

    nada que mergear

--- cliente/src/services/book-service.js
+++ cliente/src/services/book-service.js
@@ -17,45 +17,46 @@ export default class services {
   }
 
 
-  postNewPage = page => {
+  postNewPage = (id, page) => {
     return this.service.post(`/addPage/${id}`, page)
       .then(res => res.data)
       .catch(err => console.log(err))
   }
+}
 
 
 
 /* getGallery = () => {
 
-  return this.service.get('/')
-    .then(res => res.data)
-    .catch(err => console.log('Error', err))
+ return this.service.get('/')
+   .then(res => res.data)
+   .catch(err => console.log('Error', err))
 }
 
 
 getImageGallery = id => {
-  return this.service.get(`/${id}`)
-    .then(res => res.data)
-    .catch(err => console.log('Error', err))
+ return this.service.get(`/${id}`)
+   .then(res => res.data)
+   .catch(err => console.log('Error', err))
 }
 
 
 postImageGallery = image => {
-  return this.service.post('/addImage/', image)
-    .then(res => res.data)
-    .catch(err => console.log(err))
+ return this.service.post('/addImage/', image)
+   .then(res => res.data)
+   .catch(err => console.log(err))
 }
 
 postImagePage = image => {
-  return this.service.post('/addImagePage', image)
-    .then(res => res.data)
-    .catch(err => console.log(err))
+ return this.service.post('/addImagePage', image)
+   .then(res => res.data)
+   .catch(err => console.log(err))
 }
 
 handleUpload = theFile => {
 
-  return this.service.post('/upload', theFile)
-    .then(res => res.data)
-    .catch(err => console.log(err));
+ return this.service.post('/upload', theFile)
+   .then(res => res.data)
+   .catch(err => console.log(err));
 }
 } */
