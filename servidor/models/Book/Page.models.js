48149575f5fec89726efd9e90e8d124479d617f2
lulastray <luciaastray@gmail.com>
2019-06-16 17:35:19 +0200

    algo

--- servidor/models/Book/Page.models.js
+++ servidor/models/Book/Page.models.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const PageSchema = new Schema({
-  book: { type: Schema.Types.ObjectId, ref: 'Book' },
+  book_id: { type: Schema.Types.ObjectId, ref: 'Book' },
   texts: [{ type: Schema.Types.ObjectId, ref: 'TextPage' }],
   imageBackground: { type: Schema.Types.ObjectId, ref: 'ImagePage' },
   imageCharacter: [{ type: Schema.Types.ObjectId, ref: 'ImagePage' }],
