8c6a76be0e85b39ad1e892fcd17e73e0cac6da35
lulastray <luciaastray@gmail.com>
2019-06-18 15:45:12 +0200

    deploy

---
+++ servidor/public/precache-manifest.ec29dbd488887e6ac2880df8e2dc6720.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ed97860fa17cd6e4520c35db872621a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6edf7536d881ea1bc443",
+    "url": "/static/css/main.c6060372.chunk.css"
+  },
+  {
+    "revision": "ea6f52a1507a4f1827a3",
+    "url": "/static/js/2.389c5591.chunk.js"
+  },
+  {
+    "revision": "6edf7536d881ea1bc443",
+    "url": "/static/js/main.ba38a736.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
