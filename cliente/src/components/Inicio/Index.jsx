985a3bdadd29465249ec9b1cd2bc0da6313a6b38
lulastray <luciaastray@gmail.com>
2019-06-19 18:32:40 +0200

    css

--- cliente/src/components/Inicio/Index.jsx
+++ cliente/src/components/Inicio/Index.jsx
@@ -5,9 +5,16 @@ export class Index extends Component {
     render() {
         return (
             <div>
-                <div className='explain-book'>
+                <div className='flex-container-index'>
                     <figure >
-                        <img src="https://res.cloudinary.com/lulas/image/upload/v1560246748/ilustraciones/fondos%20paisajes/montan%CC%83as-tonos-naranja_labxqa.jpg" alt="montañas" />
+                        <p className="txt-index">
+                        Mucho Cuento te permite crear
+                        tu propio cuento desde cero.
+                        elige tu imagen de fondo, tus 
+                        personajes y empieza a escribir
+                        tu historia
+                        </p>        
+                        <img className="img-index" src="https://res.cloudinary.com/lulas/image/upload/v1560959810/Galeria-cuentos/nin%CC%83a-en-libro.png.png" alt="imagen niña escribiendo dentro de un libro" />
                     </figure>
 
                 </div>
